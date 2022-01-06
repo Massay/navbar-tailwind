8cb38d4ea1a5ed87fd5fbd454e6a33262a163468
Massay Bah <massaybah@gmail.com>
2022-01-06 18:35:07 +0000

    fix hamburger menu and add a close icon when menu is displayed

--- src/layout/AppLayout3.vue
+++ src/layout/AppLayout3.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sm:container sm:mx-auto  h-screen">
+  <div class="sm:container sm:mx-auto h-screen">
     <header>
       <nav class="fixed left-0 right-0 top-0 z-50 bg-green-600">
         <!-- {{ state.windowWidth }} -->
@@ -18,16 +18,30 @@
         <!-- Mobile Hambuger  -->
         <input type="checkbox" id="nav__checkbox" />
         <div class="flex justify-end">
-          <label for="nav__checkbox">
+          <label for="nav__checkbox" class="sm:hidden">
             <svg
               viewBox="0 0 100 80"
               width="40"
               height="40"
-              class="sm:hidden p-2"
+              class="p-2 open"
             >
               <rect width="100" height="20"></rect>
               <rect y="30" width="100" height="20"></rect>
               <rect y="60" width="100" height="20"></rect>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-11 w-11 close p-2 font-extrabold"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </label>
         </div>
@@ -112,7 +126,18 @@
 
     <div
       id="wrapper"
-      class="grid grid-cols-1 relative sm:grid-cols-2 md:grid-cols-4 sm:mt-16 p-2 sm:p-0 gap-3 sm:gap-0  mt-10 "
+      class="
+        grid grid-cols-1
+        relative
+        sm:grid-cols-2
+        md:grid-cols-4
+        sm:mt-16
+        p-2
+        sm:p-0
+        gap-3
+        sm:gap-0
+        mt-10
+      "
     >
       <div class="h-48 bg-purple-300">div 1</div>
       <div class="h-48 bg-gray-200">div 2</div>
@@ -121,12 +146,28 @@
       <div class="h-48 bg-blue-200">div 5</div>
       <div class="h-48 bg-green-200">div 6</div>
 
-      <div class="bg-red-400 w-10 h-10 rounded-full text-center flex items-center justify-center p-2 sm:p-8 my-2 fixed bottom-6 sm:bottom-16 right-4"> 
+      <div
+        class="
+          bg-red-400
+          w-10
+          h-10
+          rounded-full
+          text-center
+          flex
+          items-center
+          justify-center
+          p-2
+          sm:p-8
+          my-2
+          fixed
+          bottom-6
+          sm:bottom-16
+          right-4
+        "
+      >
         Icon
+      </div>
     </div>
-    </div>
-
-    
   </div>
 </template>
 
@@ -136,13 +177,23 @@
 #nav__checkbox {
   display: none;
 }
-
+.close{
+  display: none;
+}
 .mobileMenu ul {
   display: none;
 }
 
 #nav__checkbox:checked ~ .mobileMenu ul {
   display: flex;
+}
+
+#nav__checkbox:checked ~ div label .open {
+  display: none;
+}
+
+#nav__checkbox:checked ~ div  label .close {
+  display: block;
 }
 </style>
 
